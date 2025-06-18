f4ba33d57a2e61236e1725ca0084ebf21570f1dc
Louis Fernando <ferlouis55@gmail.com>
2025-06-18 20:44:07 +0700

    Fix update

--- AbsenIn/CameraPreview.swift
+++ AbsenIn/CameraPreview.swift
@@ -2,49 +2,12 @@
 //  CameraPreview.swift
 //  AbsenIn
 //
-//  Created by Hayya U on 17/06/25.
+//  Created by Louis Fernando on 18/06/25.
 //
 
-import Foundation
 import SwiftUI
 import AVFoundation
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-struct CameraPreview: UIViewRepresentable {
-    
-    let session: AVCaptureSession
-    
-    func makeUIView(context: Context) -> UIView {
-        let view = UIView()
-        view.backgroundColor = .black
-        
-        let preview_layer = AVCaptureVideoPreviewLayer(session: session)
-        preview_layer.videoGravity = .resizeAspectFill
-        preview_layer.connection?.videoOrientation = .portrait
-        
-        view.layer.addSublayer(preview_layer)
-        
-        // Pastikan layer mengikuti ukuran view
-        DispatchQueue.main.async {
-            preview_layer.frame = view.bounds
-        }
-        
-        return view
-    }
-    
-    func updateUIView(_ uiView: UIView, context: Context) {
-        // Update view jika ada perubahan state di SwiftUI
-        // Untuk preview sederhana, kita bisa biarkan kosong
-        // Atau kita bisa pastikan lagi frame nya benar
-        if let preview_layer = uiView.layer.sublayers?.first as? AVCaptureVideoPreviewLayer {
-            preview_layer.frame = uiView.bounds
-        }
-    }
-}
-=======
-=======
->>>>>>> Stashed changes
 // UIView kustom yang tahu cara me-layout layer-nya.
 class CameraPreviewUIView: UIView {
     
@@ -81,4 +44,3 @@ struct CameraPreview: UIViewRepresentable {
     
     func updateUIView(_ uiView: CameraPreviewUIView, context: Context) {}
 }
->>>>>>> Stashed changes
